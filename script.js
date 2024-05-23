48ce703312bd367a42c7b58f21c9c6005016e87b
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2024-05-23 16:15:19 +0200

    implement password validation with js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const password = document.querySelector('#password');
 const passwordConfirm = document.querySelector('#confirm_password');
-const form = document.querySelector('form');
-const passwordHint = document.createElement('span');
-passwordHint.textContent="*Passwords do not match";
-const submitInput = document.querySelector('#submitButton');
+const main = document.querySelector('main');
+const passwordNotValid = document.createElement('div');
+passwordNotValid.setAttribute("id","passwordNotValid");
+passwordNotValid.textContent="* Passwords do not match";
+main.appendChild(passwordNotValid); 
 
-
-submitInput.addEventListener('click', () => {
-    if(password!==passwordConfirm){
-        console.log("test");
-    } 
+passwordConfirm.addEventListener('keyup', () => {
+    if(password.value === passwordConfirm.value){
+        passwordNotValid.textContent="";
+    };
 });
-
